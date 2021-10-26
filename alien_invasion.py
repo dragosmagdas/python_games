d55ab8f3f4244e60b868372f663d1dd60265e8f5
Dragos Magdas <dragos.magdas@gmail.com>
2021-10-26 11:08:07 +0300

    text adventure

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,4 @@
 aliens = 2
-password = aliens
 print("quickly! aliens are invading the planet.")
 print("hope you know the password for earths sake")
 print()
@@ -7,8 +6,8 @@ print("____________________________________________")
 print("      WELCOME TO GLOBAL DEFENSE NETWORK     ")
 print("____________________________________________")
 print()
-guess = input("please enter the password: ").upper()
-while guess != password:
+guess = int(input("please enter the password:  "))
+while guess != aliens:
     print()
     print("INCORECT PASSWORD.")
     print()
@@ -19,7 +18,7 @@ while guess != password:
     print()
     print("password hint: the number of things that are attacking us")
     print()
-    guess = input("quick! please enter the password: ").upper()
+    guess = int(input("quick! please enter the password: "))
 
 if aliens > 7400000000:
     print("nooooo the aliens have outnumbered us all is lost")
